ea96cb5ed0aa2a0c926a954f375093e2f9ba46d5
lenovo <lenovo@192.168.1.2>
2013-12-29 15:45:14 +0300

    класс MacroCommand

---
+++ src/patterncommanda/MacroCommand.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package patterncommanda;
+
+/**
+ *
+ * @author lenovo
+ */
+public class MacroCommand {
+    Command[] commands;
+    
+    public MacroCommand(Command[] commands) {
+        this.commands = commands;
+    }
+    
+    public void execute() {
+        for (int i = 0; i < commands.length; i++) {
+            commands[i].execute();
+        }
+    }
+    
+    public void undo() {
+        for (int i = 0; i < commands.length; i++) {
+            commands[i].undo();
+        }
+    }
+
+}
